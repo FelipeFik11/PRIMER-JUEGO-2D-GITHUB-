5066686ed012f851cdea3232e05878a61a26454e
AnxietyFilledRaccoon <dedominicifelipe@gmail.com>
2026-03-28 20:16:20 -0300

    Pantalla frontal

--- main.gd
+++ main.gd
@@ -10,10 +10,13 @@ func _ready():
 func Game_Over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_mob_timer_timeout():
 	# Create a new instance of the Mob scene.
@@ -39,6 +42,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
